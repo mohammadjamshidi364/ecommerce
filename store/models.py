95c53f52fae16ea5d2b1dbb5c4d17b753fb1ba60
mohammad <mohammadjamshidi364@gmail.com>
2022-11-30 22:10:21 +0330

    add some function to models

--- store/models.py
+++ store/models.py
@@ -16,6 +16,13 @@ class Product(models.Model):
     image = models.ImageField(null=True , blank=True)
     def __str__(self):
         return self.name
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ""
+        return url
     
 class Order(models.Model):
     customer = models.ForeignKey(Customer , on_delete=models.SET_NULL , null=True)   
@@ -26,6 +33,18 @@ class Order(models.Model):
     def __str__(self):
         return str(self.id)
     
+    @property
+    def get_cart_total(self):
+        order_items = self.orderitem_set.all()
+        total = sum([item.get_total for item in order_items])
+        return total
+    
+    @property
+    def get_cart_items(self):
+        order_items = self.orderitem_set.all()
+        total = sum([item.quantity for item in order_items])
+        return total
+    
 class OrderItem(models.Model):
     product = models.ForeignKey(Product , on_delete=models.SET_NULL , null=True)
     order = models.ForeignKey(Order , on_delete=models.SET_NULL , null=True)
@@ -35,6 +54,10 @@ class OrderItem(models.Model):
     def __str__(self):
         return self.product.name
     
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
     
 class ShippingAddress(models.Model):
     customer = models.ForeignKey(Customer , on_delete=models.SET_NULL , null=True)
